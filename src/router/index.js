d22de9ba7aa0bb750df3582102546c753ef43077
sprajapati@gammastack.com <sprajapati@gammastack.com>
2022-02-01 13:27:12 +0530

    Updated router

--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,17 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
+  }, {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }, {
+    path: '/SportAndLeagueList',
+    name: 'SportAndLeagueList',
+    component: () => import('../views/SportAndLeagueList.vue')
   }
 ]
 
