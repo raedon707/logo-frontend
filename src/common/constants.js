6fe26a3c2c6d57483e49d1c15c10f44129d6d6ce
sprajapati@gammastack.com <sprajapati@gammastack.com>
2022-02-18 17:34:05 +0530

    Updated constants

---
+++ src/common/constants.js
@@ -0,0 +1,28 @@
+export const S3_BUCKT_PATH = {
+  FLAGS: 'flags',
+  SPORTS: 'sports',
+  LEAGUES: 'leagues',
+  PARTICIPANTS: 'participants'
+}
+
+export const ICON_UPLOAD_TYPE = {
+  LEAGUE: 'league',
+  PARTICIPANT: 'participant'
+}
+
+export const SNACKBAR_TYPE = {
+  ERROR: {
+    text: 'error',
+    value: 'red lighten-2'
+  },
+  SUCCESS: {
+    text: 'success',
+    value: 'primary'
+  }
+}
+
+export const SEARCH_SELECT_LABELS = {
+  SPORTS: 'Sports',
+  LEAGUES: 'Leagues',
+  LOCATIONS: 'Locations'
+}
